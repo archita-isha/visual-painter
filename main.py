38397ebce0e32b0ff2cec5bc120d1dad61771bb1
Archita Isha <156795223+archita-isha@users.noreply.github.com>
2026-03-11 13:14:30 +0000

    code writen but camera not opening

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import cv2
 import mediapipe as mp 
+
+cap = cv2.VideoCapture(0)  # 0= default laptop camera 
+
+while True:
+    success , frame = cap.read()
+    cv2.imshow("Camera",frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()    
+
